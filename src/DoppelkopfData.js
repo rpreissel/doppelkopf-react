bfad2dbe4dbe7f1d0b3e42a2a93d578319197ac4
Rene Preissel <rene.preissel@etosquare.de>
2015-08-05 14:36:27 +0200

    Dynaische Spieleranzahl + Namens Eingabe

--- src/DoppelkopfData.js
+++ src/DoppelkopfData.js
@@ -1,7 +1,12 @@
 
 export default class DoppelkopfData {
   constructor() {
-    this.fuenfspieler=false
+    this.fuenfspieler=false;
+    this.spieler1='Spieler1';
+    this.spieler2='Spieler2';
+    this.spieler3='Spieler3';
+    this.spieler4='Spieler4';
+    this.spieler5='Spieler5';
   }
 
 }
--- src/Spielerauswahl.js
+++ src/Spielerauswahl.js
@@ -17,15 +17,34 @@ export default class Spielerauswahl extends React.Component {
     this.emitStateChanged();
   }
 
+  spielerNameChanged(spieler,name) {
+    this.props.data[spieler]=name;
+    this.emitStateChanged();
+  }
+
   render() {
+    const playerCount=this.props.data.fuenfspieler ? 5 : 4;
+    var players = [];
+    for (var i = 1; i <= playerCount; i++) {
+      const playerVar='spieler'+i;
+      players.push(
+          <div>
+            Spieler {i}
+            <input type="text" value={this.props.data[playerVar]} onChange={(event) => this.spielerNameChanged(playerVar,event.target.value)}/>
+          </div>
+      );
+    }
+
     return (
       <div>
         <h2>
         Spielerauswahl
         </h2>
-
+        <div>
           <input type="checkbox" name="spieler" value="4" checked={!this.props.data.fuenfspieler} onChange={() => this.anzahlSpielerChanged(false)}/> 4 Spieler
           <input type="checkbox" name="spieler" value="5" checked={this.props.data.fuenfspieler} onChange={() => this.anzahlSpielerChanged(true)}/> 5 Spieler
+        </div>
+        {players}
 
         <div>
           <Link to="main">Back</Link>
